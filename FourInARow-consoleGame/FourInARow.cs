570458ff9a2cef9714d5af11e6a0138770927bf5
ThomasSoerensen1996 <thomas10001@live.dk>
2018-10-12 10:22:18 +0200

    Lavet intro & board

--- FourInARow-consoleGame/FourInARow.cs
+++ FourInARow-consoleGame/FourInARow.cs
@@ -8,10 +8,35 @@ namespace FourInARow_consoleGame
 {
     class FourInARow
     {
+
+      
+        private Player currentPlayer;
+
         char[,] twoDimensionalArray = new char[6,6];
 
+        public FourInARow()
+        {
+            
+           // Intro();
+            Console.WriteLine("+-----------------------+");
+            for (int i = 0; i < 6; i++) Console.WriteLine("|   |   |   |   |   |   |");
+            Console.WriteLine("+-----------------------+");
+        }
+
+        private static void Intro()
+        {
+            Console.WriteLine("Velkommen til Fire På Stribe!");
+            Console.WriteLine("Hvilket navn har spiller 1 idag?");
+            string navn = Console.ReadLine();
+            Player player1 = new Player('R', navn);
+            Console.WriteLine("Hvilket navn har spiller 2 idag?");
+            navn = Console.ReadLine();
+            Player player2 = new Player('B', navn);
+        }
+
+
         /// <summary>
-        /// get a singel value from our two dimensional array
+        /// get a single value from our two dimensional array
         /// </summary>
         /// <param name="x">x axis</param>
         /// <param name="y">y axis</param>
@@ -22,13 +47,13 @@ namespace FourInARow_consoleGame
         }
 
         /// <summary>
-        /// Set a single value our two dimensional array
+        /// Set a single value for our two dimensional array
         /// </summary>
         /// <param name="x">x axis position</param>
         /// <param name="y">y axis position</param>
         /// <param name="value">the char you want to set</param>
         /// <returns></returns>
-        public char setSingleValue(int x, int y, char value)
+        public void setSingleValue(int x, int y, char value)
         {
              twoDimensionalArray[x, y] = value;
         }
---
+++ FourInARow-consoleGame/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourInARow_consoleGame
+{
+    class Player
+    {
+        private string navn;
+        private char brik;
+        public Player(char brik, string navn)
+        {
+            this.brik = brik;
+            this.navn = navn;
+        }
+
+    }
+}
