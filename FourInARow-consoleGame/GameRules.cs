e78151b5d70d94bf958062cf8482e61fc2fac4a2
Morten <morto1995@hotmail.com>
2018-10-14 13:51:50 +0200

    CheckColumn() and CheckRow() code is optimized!

--- FourInARow-consoleGame/GameRules.cs
+++ FourInARow-consoleGame/GameRules.cs
@@ -28,14 +28,10 @@ namespace FourInARow_consoleGame
         {
             for (int j = 3; j <= 5; j++)
             {
-                if (game.twoDimensionalArray[j, i] == game.currentPlayer.brik
-                    && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j - 1, i + 1]
-                    && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j - 2, i + 2]
-                    && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j - 3, i + 3])
+                if (game.twoDimensionalArray[j, i] == game.currentPlayer.brik && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j - 1, i + 1] && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j - 2, i + 2] && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j - 3, i + 3])
                 {
                     game.gameOver = true;
                     Console.WriteLine(game.currentPlayer.navn + " har vundet");
-                    break;
                 }
             }
         }
@@ -43,27 +39,20 @@ namespace FourInARow_consoleGame
         {
             for (int j = 0; j <= 2; j++)
             {
-                if (game.twoDimensionalArray[j, i] == game.currentPlayer.brik
-                    && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j + 1, i + 1]
-                    && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j + 2, i + 2]
-                    && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j + 3, i + 3])
+                if (game.twoDimensionalArray[j, i] == game.currentPlayer.brik && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j + 1, i + 1] && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j + 2, i + 2] && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j + 3, i + 3])
                 {
                     game.gameOver = true;
                     Console.WriteLine(game.currentPlayer.navn + " har vundet");
-                    break;
                 }
             }
         }
         public void CheckRow()
         {
-            for (int j = 0; j < 6; j++)
+            for (int j = 0; j <= 2; j++)
             {
-                for (int i = 0; i < 6; i++)
+                for (int i = 0; i <= 5; i++)
                 {
-                    if (game.twoDimensionalArray[j, i] == game.currentPlayer.brik) counter++;
-                    else counter = 0;
-
-                    if (counter == 4)
+                    if (game.twoDimensionalArray[j, i] == game.currentPlayer.brik && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j + 1, i] && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j + 2, i] && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j + 3, i])
                     {
                         game.gameOver = true;
                         Console.WriteLine(game.currentPlayer.navn + " har vundet");
@@ -73,14 +62,11 @@ namespace FourInARow_consoleGame
         }
         public void CheckColumn()
         {
-            for (int j = 0; j < 6; j++)
+            for (int j = 0; j <= 5; j++)
             {
-                for (int i = 0; i < 6; i++)
+                for (int i = 0; i <= 2; i++)
                 {
-                    if (game.twoDimensionalArray[i, j] == game.currentPlayer.brik) counter++;
-                    else counter = 0;
-
-                    if (counter == 4)
+                    if (game.twoDimensionalArray[j, i] == game.currentPlayer.brik && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j, i + 1] && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j, i + 2] && game.twoDimensionalArray[j, i] == game.twoDimensionalArray[j, i + 3])
                     {
                         game.gameOver = true;
                         Console.WriteLine(game.currentPlayer.navn + " har vundet");
